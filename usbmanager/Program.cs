bd4e888642879b05cf26476a19563a9cd479b25b
Programacion2 <Programacion2@DESKTOP-AIRUGOP>
2017-02-22 10:27:38 +0100

    Agregar archivos de proyecto.

---
+++ usbmanager/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace usbmanager
+{
+    using System;
+    using System.IO;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using static DeviceManager;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length==1)
+            {
+                switch (args[0])
+                {
+                    case "revisar":
+                        RevisarFichero();
+                        break;
+                    case "montar":
+                        Montar();
+                        break;
+                    case "desmontar":
+                        Desmontar();
+                        break;
+                }
+            }
+        }
+
+        static void Desmontar()
+        {
+            var config = ReadXML();
+            Guid mouseGuid = new Guid(config.GUID);
+            string instancePath = @config.PATH.Replace("&amp;", "&");
+            DeviceHelper.SetDeviceEnabled(mouseGuid, instancePath, false);
+        }
+
+        static void Montar()
+        {
+            var config = ReadXML();
+            Guid mouseGuid = new Guid(config.GUID);
+            string instancePath = @config.PATH.Replace("&amp;","&");
+            DeviceHelper.SetDeviceEnabled(mouseGuid, instancePath, true);
+        }
+        static void RevisarFichero()
+        {
+            string text = File.ReadAllText("configuracion.xml");
+            text = text.Replace("&", "&amp;");
+            File.WriteAllText("configuracion.xml", text);
+        }
+
+
+        static configuracion ReadXML()
+        {
+            string rutaConfig = AppDomain.CurrentDomain.BaseDirectory + "configuracion.xml";
+            if (File.Exists(rutaConfig))
+            {
+                XmlSerializer _s = new XmlSerializer(typeof(configuracion));
+                var tempConf = (configuracion)_s.Deserialize(new XmlTextReader(rutaConfig));
+                return tempConf;
+            }
+            else
+            {
+                Console.WriteLine("FICHERO DE CONFIGURACION INEXISTENTE, Pulse cualquier tecla para salir");
+                Console.ReadKey();
+                Environment.Exit(0);
+                return null;
+            }
+        }
+    }
+}
---
+++ usbmanager/configuracion.cs
@@ -0,0 +1,8 @@
+﻿namespace usbmanager
+{
+    public class configuracion 
+    {
+        public string GUID { get; set; }
+        public string PATH { get; set; }
+    }
+}
